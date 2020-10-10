e847078eb748d7d424fd590da2b651b89384d04b
Igor Shchenev <shenshen@yandex.ru>
2020-10-10 12:25:41 +0300

    add comments and fix errors

--- HW02 - select/query_dz2.sql
+++ HW02 - select/query_dz2.sql
@@ -1,4 +1,8 @@
 --1
+/*
+Все товары, в названии которых есть "urgent" или название начинается с "Animal". Вывести: ИД товара, наименование товара.
+Таблицы: Warehouse.StockItems.
+*/
 select
 	wsi.StockItemID,
 	wsi.StockItemName
@@ -10,6 +14,10 @@ where
 
 
 --2
+/*
+Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders). Сделать через JOIN, с подзапросом задание принято не будет. Вывести: ИД поставщика, наименование поставщика.
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders.
+*/
 select
 	ps.SupplierID,
 	ps.SupplierName
@@ -22,6 +30,18 @@ where
 
 
 --3
+/*
+Заказы (Orders) с ценой товара более 100$ либо количеством единиц товара более 20 штук и присутствующей датой комплектации всего заказа (PickingCompletedWhen).
+Вывести:
+* OrderID
+* дату заказа в формате ДД.ММ.ГГГГ
+* название месяца, в котором была продажа
+* номер квартала, к которому относится продажа
+* треть года, к которой относится дата продажи (каждая треть по 4 месяца)
+* имя заказчика (Customer)
+Добавьте вариант этого запроса с постраничной выборкой, пропустив первую 1000 и отобразив следующие 100 записей. Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
+Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
+*/
 select
 	so.OrderID,
 	convert(varchar, so.OrderDate, 104) as OrderDateDMY,
@@ -64,6 +84,16 @@ offset 1000 rows fetch next 100 rows only;
 
 
 --4
+/*
+Заказы поставщикам (Purchasing.Suppliers), которые были исполнены в январе 2014 года с доставкой Air Freight или Refrigerated Air Freight (DeliveryMethodName).
+Вывести:
+* способ доставки (DeliveryMethodName)
+* дата доставки
+* имя поставщика
+* имя контактного лица принимавшего заказ (ContactPerson)
+
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
+*/
 select
 	adm.DeliveryMethodName,
 	ppo.ExpectedDeliveryDate,
@@ -81,6 +111,9 @@ where
 
 
 --5
+/*
+Десять последних продаж (по дате) с именем клиента и именем сотрудника, который оформил заказ (SalespersonPerson).
+*/
 select top 10
 	so.OrderDate,
 	sc.CustomerName,
@@ -94,6 +127,9 @@ order by so.OrderDate desc;
 
 
 --6
+/*
+Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g. Имя товара смотреть в Warehouse.StockItems.
+*/
 select
 	sc.CustomerID,
 	sc.CustomerName,
@@ -109,11 +145,22 @@ where
 
 
 --7
+/*
+Посчитать среднюю цену товара, общую сумму продажи по месяцам
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Средняя цена за месяц по всем товарам
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
 select
 	year(si.InvoiceDate) as year_invoicedate,
 	month(si.InvoiceDate) as month_invoicedate,
-	avg(sil.ExtendedPrice) as avg_price,
-	sum(sil.ExtendedPrice) as sum_price
+	avg(sil.UnitPrice) as avg_price,
+	sum(sil.Quantity * sil.UnitPrice) as sum_price
 from
 	Sales.Invoices si
 		join Sales.InvoiceLines sil on sil.InvoiceID = si.InvoiceID
@@ -123,25 +170,49 @@ order by year_invoicedate, month_invoicedate;
 
 
 --8
+/*
+Отобразить все месяцы, где общая сумма продаж превысила 10 000
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
 select
 	year(si.InvoiceDate) as year_invoicedate,
 	month(si.InvoiceDate) as month_invoicedate,
-	sum(sil.ExtendedPrice) as sum_price
+	sum(sil.Quantity * sil.UnitPrice) as sum_price
 from
 	Sales.Invoices si
 		join Sales.InvoiceLines sil on sil.InvoiceID = si.InvoiceID
 group by year(si.InvoiceDate), month(si.InvoiceDate)
-having sum(sil.ExtendedPrice) > 10000
+having sum(sil.Quantity * sil.UnitPrice) > 10000
 order by year_invoicedate, month_invoicedate;
 
 
 
 --9
+/*
+Вывести сумму продаж, дату первой продажи и количество проданного по месяцам, по товарам, продажи которых менее 50 ед в месяц.
+Группировка должна быть по году, месяцу, товару.
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Наименование товара
+* Сумма продаж
+* Дата первой продажи
+* Количество проданного
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
 select
 	year(si.InvoiceDate) as year_invoicedate,
 	month(si.InvoiceDate) as month_invoicedate,
 	wsi.StockItemName,
-	sum(sil.ExtendedPrice) as sum_price,
+	sum(sil.Quantity * sil.UnitPrice) as sum_price,
 	min(si.InvoiceDate) as first_sales,
 	sum(sil.Quantity) as sum_quantity
 from
